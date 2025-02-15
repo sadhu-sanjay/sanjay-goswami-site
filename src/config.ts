5001d10e8dffd761eaf390de1620465a970312f0
sadhu-sanjay <sanjaygoswami60@gmail.com>
2025-02-15 16:23:30 +0530

    First commit

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Sanjay Goswami | Sr. Software Engineer | React Rust Raspberrypi';
+export const SITE_DESCRIPTION = 'Personal Website of Sanjay Goswami';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
